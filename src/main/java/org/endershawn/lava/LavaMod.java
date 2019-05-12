827fe3852940f19209d59a715787a9614b05ddb0
BuildTools <unconfigured@null.spigotmc.org>
2019-05-12 12:33:06 -0700

    Cleanup

--- src/main/java/org/endershawn/lava/LavaMod.java
+++ src/main/java/org/endershawn/lava/LavaMod.java
@@ -1,17 +1,9 @@
 package org.endershawn.lava;
 
-import net.minecraft.block.Block;
-import net.minecraft.init.Blocks;
-import net.minecraft.item.Item;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
@@ -44,25 +36,6 @@ public class LavaMod
         // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
     }
-
-//    private void doClientStuff(final FMLClientSetupEvent event) {
-//        // do something that can only be done on the client
-//        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
-//    }
-//
-//    private void enqueueIMC(final InterModEnqueueEvent event)
-//    {
-//        // some example code to dispatch IMC to another mod
-//        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
-//    }
-//
-//    private void processIMC(final InterModProcessEvent event)
-//    {
-//        // some example code to receive and process InterModComms from other mods
-//        LOGGER.info("Got IMC {}", event.getIMCStream().
-//                map(m->m.getMessageSupplier().get()).
-//                collect(Collectors.toList()));
-//    }
     
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
--- src/main/java/org/endershawn/lava/item/ModItems.java
+++ src/main/java/org/endershawn/lava/item/ModItems.java
@@ -24,23 +24,6 @@ public class ModItems {
 	private static final Logger LOGGER = LogManager.getLogger();
 	public static final IItemTier lavaTier = new LavaTier();
 	public static final IItemTier lightningTier = new LightningTier();
-
-//	public static final Set<Block> LAVA_EFFECTIVE_ON = Sets.newHashSet(Blocks.STONE_BRICK_STAIRS, Blocks.STONE_SLAB, Blocks.STONE_SLAB2, Blocks.SAND, Blocks.STONE, Blocks.TALLGRASS, Blocks.LEAVES2, Blocks.CLAY, Blocks.DIRT, Blocks.FARMLAND, Blocks.GRASS, Blocks.GRAVEL, Blocks.MYCELIUM, Blocks.SAND, Blocks.SANDSTONE, Blocks.SANDSTONE_STAIRS, Blocks.RED_SANDSTONE, Blocks.RED_SANDSTONE_STAIRS, Blocks.SNOW, Blocks.SNOW_LAYER, Blocks.SOUL_SAND, Blocks.GRASS_PATH, Blocks.CONCRETE_POWDER, Blocks.GRASS, Blocks.LEAVES, Blocks.PLANKS, Blocks.BOOKSHELF, Blocks.LOG, Blocks.LOG2, Blocks.CHEST, Blocks.PUMPKIN, Blocks.LIT_PUMPKIN, Blocks.MELON_BLOCK, Blocks.LADDER, Blocks.WOODEN_BUTTON, Blocks.WOODEN_PRESSURE_PLATE);
-//
-//	public static final ToolMaterial FIRE = EnumHelper.addToolMaterial("LAVA", 3, 700, 100f, 6f, 30);
-//	public static final Set<Block> FIRE_EFFECTIVE_ON = Sets.newHashSet(Blocks.TALLGRASS, Blocks.LEAVES2, Blocks.CLAY, Blocks.DIRT, Blocks.FARMLAND, Blocks.GRASS, Blocks.GRAVEL, Blocks.MYCELIUM, Blocks.SNOW, Blocks.SNOW_LAYER, Blocks.SOUL_SAND, Blocks.GRASS_PATH, Blocks.GRASS, Blocks.LEAVES, Blocks.PLANKS, Blocks.BOOKSHELF, Blocks.LOG, Blocks.LOG2, Blocks.CHEST, Blocks.PUMPKIN, Blocks.LIT_PUMPKIN, Blocks.MELON_BLOCK, Blocks.LADDER, Blocks.WOODEN_BUTTON, Blocks.WOODEN_PRESSURE_PLATE);
-//	
-//	
-//	public static final ToolMaterial THUNDER = EnumHelper.addToolMaterial("THUNDER", 3, 3000, 100f, Float.MAX_VALUE, 30);
-//	public static final Set<Block> THUNDER_EFFECTIVE_ON = Sets.newHashSet(Blocks.TALLGRASS, Blocks.LEAVES2, Blocks.CLAY, Blocks.DIRT, Blocks.FARMLAND, Blocks.GRASS, Blocks.GRAVEL, Blocks.MYCELIUM, Blocks.SAND, Blocks.SNOW, Blocks.SNOW_LAYER, Blocks.SOUL_SAND, Blocks.GRASS_PATH, Blocks.CONCRETE_POWDER, Blocks.GRASS, Blocks.LEAVES, Blocks.PLANKS, Blocks.BOOKSHELF, Blocks.LOG, Blocks.LOG2, Blocks.CHEST, Blocks.PUMPKIN, Blocks.LIT_PUMPKIN, Blocks.MELON_BLOCK, Blocks.LADDER, Blocks.WOODEN_BUTTON, Blocks.WOODEN_PRESSURE_PLATE);
-//
-//	public static SwordLava lavaSword = new SwordLava("sword_lava");
-//	public static HammerFire fireHammer = new HammerFire("hammer_fire");
-//	public static SwordThunder thunderSword = 
-//			(SwordThunder) new SwordThunder("sword_thunder")
-//				.setRegistryName(LavaMod.MODID, "sword_thunder");
-//	public static Sithe sithe = new Sithe("sithe");
-
 	
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event) {
@@ -52,38 +35,4 @@ public class ModItems {
         		new Sithe()
 		);
     }
-	
-    
-//	public static void register(IForgeRegistry<Item> registry) {
-//		registry.registerAll(thunderSword);
-//	}
-//	
-	
-//	@Mod.EventBusSubscriber
-//    public static class DamageHandler {
-//    	private static boolean isFireDamage(DamageSource s) {
-//    		return (s.equals(DamageSource.LAVA) 
-//            	    || s.equals(DamageSource.IN_FIRE) 
-//            	    || s.equals(DamageSource.ON_FIRE));
-//    	}
-//    	
-//    	private static boolean playerHoldingLava(EntityPlayer p) {
-//    		return p.getHeldItemMainhand().getItem() instanceof SwordLava;
-//    	}
-//    	
-//    	@SubscribeEvent
-//    	public static void playerBurning(LivingAttackEvent event) {
-//    		Entity e = event.getEntity();
-//    		DamageSource s = event.getSource();
-//    		
-//    		if (e instanceof EntityPlayer) {
-//    			EntityPlayer player = (EntityPlayer)e;
-//    			    			
-//            	if (isFireDamage(s) && playerHoldingLava(player)) {
-//            		player.extinguish();
-//            		event.setCanceled(true);
-//            	}
-//    		}
-//    	}
-//    }
 }
